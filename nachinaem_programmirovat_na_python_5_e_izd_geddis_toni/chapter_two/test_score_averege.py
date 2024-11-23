2e5d6a482649e2ba296aff88dbb0e22bc2ed1674
Avdeev Stanislav <d1g1texavd@gmail.com>
2024-11-24 00:06:27 +0300

    Программы, которая вычисляет среднее значение. Предположим, что на занятиях по информатике вы получили три оценки и хотите написать программу, которая выведет средний балл из полученных оценок.

---
+++ nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/chapter_two/test_score_averege.py
@@ -0,0 +1,10 @@
+# Получить три оценки.
+estimation_one = float(input('Введите первую оценку: '))
+estimation_two = float(input('Введите вторую оценку: '))
+estimation_tree = float(input('Введите третью оценку: '))
+
+# Вычисляем средний балл.
+averege_score = (estimation_one + estimation_two + estimation_tree) / 3.0
+
+# Показать средний балл.
+print('Средний балл составляет', averege_score)

7b70870acb98eb336b28df68dd3b4d195ce4e2b3
Avdeev Stanislav <d1g1texavd@gmail.com>
2025-01-16 15:46:26 +0300

    # Часы

--- nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/test.py
+++ nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/test.py
@@ -1,2 +1,5 @@
-import turtle
-turtle.write('Привет мир')
+for hours in range(24):
+    for minutes in range(60):
+        for seconds in range(60):
+            print(hours, ':', minutes, ':', seconds)
+

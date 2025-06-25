ea9e4ceca629c77891838cf1be162e2c18edcfd6
Avdeev Stanislav <d1g1texavd@gmail.com>
2025-06-25 18:30:50 +0300

    Cкрипт для обработки лога веб-сервера

--- practicum/webServerLogProcessing.py
+++ practicum/webServerLogProcessing.py
@@ -1,6 +1,8 @@
 # Imports.
 import tarfile
 from requests import get
+import pandas as pd
+
 
 # Link to the log file.
 url_test = "https://sysadmin.education-services.ru/downloads/nginx.log.tar.gz"
@@ -12,6 +14,8 @@ def download(url, file_name):
     with open (file_name, "wb") as file:
         response = get(url)
         file.write(response.content)
+
+download(url_test, file_name_test)
 
 # Extract the archive.
 file = tarfile.open('nginx.log.tar.gz')
@@ -26,5 +30,22 @@ with open('nginx.log', 'r') as file:
 for line in range(10):
     print(f"(line): {lines[line]}")
 
-# Колчество строк в логе.
+# Количество строк в логе.
 len(lines)
+
+# Подсчет ip адресов.
+ip_adresses = {}
+for line in range(len(lines)):
+    current_ip = lines[line].split(" - - ")[0]
+    if current_ip in ip_adresses:
+        ip_adresses[current_ip] += 1
+    else:
+        ip_adresses[current_ip] = 1
+
+len(ip_adresses)
+
+df = pd.DataFrame.from_dict(ip_adresses,orient='index')
+df.head()
+df.to_csv("./result.txt",sep=":")
+df.sort_index()
+

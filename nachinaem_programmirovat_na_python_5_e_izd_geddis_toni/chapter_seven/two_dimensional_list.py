ec14878c2b181ae2f9e18c62de7fad0929c2ce74
Avdeev Stanislav <d1g1texavd@gmail.com>
2025-08-11 13:40:26 +0300

    # Эта программа демонстрирует двумерный список.

---
+++ nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/chapter_seven/two_dimensional_list.py
@@ -0,0 +1,13 @@
+# Эта программа демонстрирует двумерный список.
+
+def main():
+    values = [[1, 2, 3],
+              [10, 20, 30],
+              [100, 200, 300]]
+
+    for row in values:
+        for element in row:
+            print(element)
+
+if __name__ == '__main__':
+    main()

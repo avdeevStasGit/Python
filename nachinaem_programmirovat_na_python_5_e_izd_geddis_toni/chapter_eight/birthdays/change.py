9bccda85e1dc45dffd6422f296d8ee0444e0e686
Avdeev Stanislav <d1g1texavd@gmail.com>
2025-11-15 21:46:30 +0300

    # Функция change изменяет существующую # запись в словаре birthdays.

---
+++ nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/chapter_eight/birthdays/change.py
@@ -0,0 +1,14 @@
+# Функция change изменяет существующую
+# запись в словаре birthdays.
+def change(birthdays):
+    #Получить искомое имя.
+    name = input('Введите имя: ')
+
+    if name in birthdays:
+        # Получить новый день рождения.
+        bday = input('Введите новый день рождения: ')
+
+        # Обновить запись.
+        birthdays[name] = bday
+    else:
+        print('Это имя не найдено.')

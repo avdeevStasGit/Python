3c1ce9a46ddb7b55e6f2fd9a1010a3394f6bc75e
Avdeev Stanislav <d1g1texavd@gmail.com>
2025-11-19 20:36:15 +0300

    # Эта программа демонстрирует различные операции над множествами.

---
+++ nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/chapter_eight/sets.py
@@ -0,0 +1,45 @@
+# Эта программа демонстрирует различные операции над множествами.
+
+baseball = set(['Джожи', 'Кармэн', 'Аида', 'Алисия'])
+basketball = set(['Ева', 'Кармэн', 'Алисия', 'Сара'])
+
+# Показать членов бейсбольного множества.
+print('Эти студенты состоят в бейсбольной команде: ')
+for name in baseball:
+    print(name)
+
+# Показать членов байскетбольного множества.
+print()
+print('Эти студенты состоят в баскетбольной команде: ')
+for name in basketball:
+    print(name)
+
+ # Продемонстрировать пересечение.
+print()
+print('Эти студенты играют в бейсбол, и в баскетбол: ')
+for name in baseball.intersection(basketball):
+    print(name)
+
+# Продемонстрировать объединение.
+print()
+print('Эти студенты играют в одну или обе спортивные игры: ')
+for name in baseball.union(basketball):
+    print(name)
+
+# Продемонстрировать разность между бейсболом и баскетболом.
+print()
+print('Эти студенты играют в бейсбол, но не в баскетбол: ')
+for name in baseball.difference(basketball):
+    print(name)
+
+# Продемонстрировать разность между баскетболом и бейсболом.
+print()
+print('Эти студенты играют в баскетбол, но не в бейсбол:')
+for name in baseball.difference(basketball):
+    print(name)
+
+# Продемонстрировать симметрическую разность.
+print()
+print('Эти студенты играют в одну из спортивных игр, но не в обе: ')
+for name in baseball.symmetric_difference(basketball):
+    print(name)

4ce4e3f5af90d600e5168250a25cd7e89a06bb94
Avdeev Stanislav <d1g1texavd@gmail.com>
2025-12-13 23:45:30 +0300

    # Класс Automobile содержит общие данные # об автомобиле на складе.

---
+++ nachinaem_programmirovat_na_python_5_e_izd_geddis_toni/chapter_ten/vehicles.py
@@ -0,0 +1,33 @@
+# Класс Automobile содержит общие данные
+# об автомобиле на складе.
+
+class Automobile:
+    def __init__(self, make, model, mileage, price):
+        self.__make = make
+        self.__model = model
+        self.__mileage = mileage
+        self.__price = price
+
+    def set_make(self, make):
+        self.__make = make
+
+    def set_model(self, model):
+        self.model = model
+
+    def set_mileage(self, mileage):
+        self.__mileage = mileage
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_make(self):
+        return self.__make
+
+    def get_model(self):
+        return self.model
+
+    def get_mileage(self):
+        return self.__mileage
+
+    def get_price(self):
+        return self.__price
